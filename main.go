92f7843bcfee2379fa2cd6950ad57d70ea0d0255
blacklad <blacklad@163.com>
2020-02-27 00:09:21 +0800

    mountnamespace测试

--- main.go
+++ main.go
@@ -12,11 +12,32 @@ func main() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 10086,
+				HostID:      syscall.Getuid(),
+				Size:        1,
+			},
+			{
+				ContainerID: 10010,
+				HostID:      syscall.Getgid() + 1,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 10086,
+				HostID:      syscall.Getgid(),
+				Size:        1,
+			},
+			{
+				ContainerID: 10010,
+				HostID:      syscall.Getgid() + 1,
+				Size:        1,
+			},
+		},
 	}
-	cmd.SysProcAttr.Credential = &syscall.Credential{
-		Uid: uint32(1),
-		Gid: uint32(1),
-	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
