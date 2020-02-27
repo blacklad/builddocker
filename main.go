6319062064f796e20a94a5b344999a84a5b582e5
blacklad <blacklad@163.com>
2020-02-27 10:15:35 +0800

    usernamespace测试

--- main.go
+++ main.go
@@ -12,11 +12,22 @@ func main() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      syscall.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      syscall.Getgid(),
+				Size:        1,
+			},
+		},
 	}
-	cmd.SysProcAttr.Credential = &syscall.Credential{
-		Uid: uint32(1),
-		Gid: uint32(1),
-	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
